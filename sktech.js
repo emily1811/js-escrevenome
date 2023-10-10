5b9ff6ce98d8254bbc1eb2a5994a8acd885c4e85
emily1811 <145472136+emily1811@users.noreply.github.com>
2023-10-10 11:44:41 -0300

    adicionando arquivo js e css

---
+++ sktech.js
@@ -0,0 +1,13 @@
+function setup() {
+    createCanvas(700, 700);
+    background("white");
+  }
+  
+  function draw() {
+    stroke("white")
+    fill("black")
+    
+   if(mouseIsPressed){ 
+     rect(mouseX,mouseY,50,50);
+     }
+  }
